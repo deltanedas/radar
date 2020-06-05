7f4dd2709a4cefe152a0fd24e544ad4318759956
DeltaNedas <deltanedas@gmail.com>
2020-06-05 01:04:30 +0100

    1.4.0: start search from last found tile

--- scripts/main.js
+++ scripts/main.js
@@ -4,21 +4,25 @@ const valid = (name, block) => {
 	return block === null ? false : block.name.includes(name);
 };
 
-var result = "", query = "";
+var result = "", query = "", lastFound = {x: 0, y: 0};
 
 const findRow = (name, x) => {
 	if (x == Vars.world.width()) {
 		result = "[red]X[]";
+		lastFound.x = lastFound.y = 0;
 		return
 	}
 
 	const tiles = Vars.world.tiles;
 	const col = tiles[x];
 	var tile;
-	for (var y = 0; y < Vars.world.height(); y++) {
+	for (var y = (x == lastFound.x ? lastFound.y : 0); y < Vars.world.height(); y++) {
 		tile = col[y];
 		if (valid(name, tile.block()) || valid(name, tile.floor())) {
 			result = "[green](" + x + ", " + y + ")[]";
+			lastFound.x = x;
+			// Prevent finding the same thing without looping over
+			lastFound.y = y + 1;
 			if (this.global.tracker) {
 				this.global.tracker.setMarker({x: x * Vars.tilesize, y: y * Vars.tilesize});
 			}
@@ -33,7 +37,9 @@ const findRow = (name, x) => {
 };
 
 const find = name => {
-	findRow(name, 0);
+	print("lastFound is " + lastFound)
+	print("lastFound.x is " + lastFound.x)
+	findRow(name, lastFound.x);
 };
 
 ui.addTable("top", "radar", radar => {
