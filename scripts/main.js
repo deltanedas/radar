a57811fbdca51cfb4846d21bd9f336ef36a714cf
DeltaNedas <deltanedas@gmail.com>
2020-06-28 17:06:26 +0100

    1.5.0: radar search for overlays (ores) too

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,30 @@
+/*
+	Copyright (c) DeltaNedas 2020
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 var ui = require("ui-lib/library");
 
-const valid = (name, block) => {
-	return block === null ? false : block.name.includes(name);
+const valid = (name, blocks) => {
+	var block;
+	for (var i in blocks) {
+		block = blocks[i];
+		if (block.name.includes(name)) {
+			return true;
+		}
+	}
+	return false;
 };
 
 var result = "", query = "", lastFound = {x: 0, y: 0};
@@ -18,7 +41,7 @@ const findRow = (name, x) => {
 	var tile;
 	for (var y = (x == lastFound.x ? lastFound.y : 0); y < Vars.world.height(); y++) {
 		tile = col[y];
-		if (valid(name, tile.block()) || valid(name, tile.floor())) {
+		if (valid(name, [tile.block(), tile.floor(), tile.overlay()])) {
 			result = "[green](" + x + ", " + y + ")[]";
 			lastFound.x = x;
 			// Prevent finding the same thing without looping over
